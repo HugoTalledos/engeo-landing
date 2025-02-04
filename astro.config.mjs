e0fb372313cf596d015bababbaccb279de161787
Hugo Talledos <hugo.talledos05@gmail.com>
2025-02-03 21:41:58 -0500

    Add preact to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import tailwindIntegration from '@astrojs/tailwind';
+import preact from '@astrojs/preact';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwindIntegration()]
+    integrations: [tailwindIntegration(), preact()]
 });
